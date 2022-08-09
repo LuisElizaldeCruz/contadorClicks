3a2e389bd2abec3215749d407e398383c1a0c3e2
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-08 22:53:12 -0500

    se agrego las funciones para ser llamadas en los botones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import logo from "./assets/imagenes/stock-photo-cup.jpeg";
+import Boton from './components/Boton';
 
 function App() {
+  const manejarClic = () => {
+    console.log("clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar")  
+  }
+
   return (
     <div className="App">
-     
+      <div className="logo-contenedor">
+        <img className="logo-imagen"
+          src={logo}
+          alt="Log de la pagina" />
+      </div>
+
+      <div className="contenedor-principal">
+        <Boton
+          texto="Clic"
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
     </div>
   );
 }
